200fd56197e0d25cfe124570f0466c45af654cc3
Clay Diffrient <clay.diffrient@gmail.com>
2013-06-29 21:03:48 -0600

    Preloading images for the heads.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
    //Preload the blue images to make it slightly faster to change them.
    var images = ['img/achievement-blue.png', 'img/affection-blue.png', 'img/anxiety-blue.png',
                  'img/demandingness-blue.png', 'img/depression-blue.png', 'img/other-blue.png',
-                 'img/submissiveness-blue.png'];
+                 'img/submissiveness-blue.png', 'img/Head_achievement.png', 'img/Head_affection.png',
+                 'img/Head_anxiety.png', 'img/Head_demandingness.png', 'img/Head_depression.png',
+                 'img/Head_other.png', 'img/Head_submissiveness.png'];
    $(images).each(function(){
       $('<img/>')[0].src = this;
    });
@@ -39,4 +41,5 @@ var clickHandle = function(target) {
    var name = src[1].split('-');
    $('#head img').attr('src', "img/Head_" + name[0] + ".png");
    $('#head .caption').text(name[0]);
+   $('#message').text("A message about " + name[0] + " goes here.");
 }
